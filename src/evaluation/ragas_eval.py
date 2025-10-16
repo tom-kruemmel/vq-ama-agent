d70ac11978feccb694002f99628370fe843dbc88
tom-kruemmel <tom@virtualq.io>
2025-10-16 11:43:25 +0200

    WIP

--- src/evaluation/ragas_eval.py
+++ src/evaluation/ragas_eval.py
@@ -463,7 +463,7 @@ def main():
     model_id = os.getenv("BEDROCK_MODEL_ID")
     aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
     aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-    region_name = os.getenv("AWS_DEFAULT_REGION", "us-east-1")
+    region_name = os.getenv("AWS_DEFAULT_REGION", "eu-central-1")
 
     session = boto3.Session(
         aws_access_key_id=aws_access_key_id,
